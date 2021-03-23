e9b57a6d572547d48b518ef5530d2a96d3b9a26c
Leon Hsu <leonhsu95@gmail.com>
2021-03-23 14:01:58 +1100

    Timer function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
-// STARTING GAME
-var startButton = document.querySelector("#startGame");
+// STARTING GAME VARIABLES
+var timeLeft = 10+1;
+var score = parseInt(timeLeft);
+var startButton = document.querySelector("#start-button");
+var homeScreen = document.querySelector("#home-screen");
 
 
 // QUESTIONS
@@ -35,17 +38,49 @@ var questions = [
  },
 
  {
-    title: "What can be expected from  === ?",
-    choices: ["The value is the same", "The datatype is the same", "Both value and datatpe are the same", "one variable is forced to equal another variable"],
-    answer: "Both value and datatpe are the same"
+    title: "What can be expected from a === b?",
+    choices: ["The values of a and b are the same", "The datatype of a and b are the same", "Both value and datatypes of of a and b are the same", "a is declared to be equal to b"],
+    answer: "Both value and datatypes of of a and b are the same"
  },
 
 ];
 
-function startGame(){
+
+function startQuiz(){
+
+   homeScreen.innerHTML= "";
+   
+
+}
+
+// TIMER
+
+function timer() {
+   var timerInterval = setInterval(function(){
+      timeLeft--;
+      document.querySelector("#seconds").textContent=timeLeft;
+
+      if(timeLeft === 0){
+         clearInterval(timerInterval);
+         
+      }
+
+
+   }, 1000);
+
+
+
+
+
+
+
+
 
 }
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", startGame);
+startButton.addEventListener("click", function(){
+   startQuiz();
+   timer();
+});
